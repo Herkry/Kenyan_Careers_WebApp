a5c34dcbaf8816f320241f702cda9c2463bfa41b
Daniel Simiyu <danielmsimiyu71@gmail.com>
2020-05-17 14:03:42 +0300

    adding plain header+footer class_and css file

---
+++ kenyan_careers_webapp/Employer/footer.php
@@ -0,0 +1,28 @@
+<?php
+class Footer
+{
+    function display_plain_footer()
+    {
+        ?>
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/header.css">
+        </head>
+
+        <body>
+        <div class="footer">
+        <p>
+        <img height="90" width="260" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/kenyacareer_vectorcopy.png" alt="kenyacareer_logo">
+                    
+        </p>
+        </div>
+        </body>
+        </html> 
+
+        <?php
+    }
+
+}
+
+?>
