32ec3a915257f5906307d48b129e5278e999ecd3
Daniel Simiyu <danielmsimiyu71@gmail.com>
2020-05-29 06:41:05 +0300

    Solved verification error, login page work on now.

--- kenyan_careers_webapp/Employer/footer.php
+++ kenyan_careers_webapp/Employer/footer.php
@@ -41,7 +41,6 @@ class Footer
         <!-- <img height="90" width="260" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/kenyacareer_vectorcopy.png" alt="kenyacareer_logo">-->
         </p>
 
-
         <div class="row">
         <div class="col-md-12">
         <a href="#"><img height="100" width="100" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/employee_act.png" alt="Employee Account"></a>
@@ -56,6 +55,44 @@ class Footer
         </html>
         <?php
 
+    }
+    function display_socialmedia_footer()
+    {
+      ?>
+      <html>
+      <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/employer_header_footer.css">
+      <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/Israel_custom.css">
+      </head>
+
+      <body>
+      <div class="footer">
+      <p>
+      <!-- <img height="90" width="260" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/kenyacareer_vectorcopy.png" alt="kenyacareer_logo">-->
+      </p>
+
+      <div class="row">
+      <div class="col-md-12">
+      <a href="#"><img height="50" width="50" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/fb_logo.png" alt="Facebook Account"></a>
+      <a href="#">Connect Facebook</a>
+      <a href="#"><img height="50" width="50" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/tw_logo.png" alt="Twitter Account"></a>
+      <a href="#">Connect Twitter</a>
+      <a href="#"><img height="50" width="50" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/ig_logo.png" alt="Instagram Account"></a>
+      <a href="#">Connect Instagram</a>
+      <a href="#"><img height="50" width="50" class="img-responsive" src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/Images/comm_logo.png" alt="Talk to us"></a>
+      <a href="#">Talk to us</a>
+      </div>
+
+
+      </div>
+
+      </div>
+      </body>
+      </html>
+      <<?php
     }
 }
 
