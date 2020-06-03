39209997b3ddd64cd4685eadd8096697023c278c
Daniel Simiyu <danielmsimiyu71@gmail.com>
2020-06-03 05:50:28 +0300

    footer and header functions update, employer landpage setup

---
+++ kenyan_careers_webapp/Employer/employer_homepage.php
@@ -0,0 +1,21 @@
+<?php
+require_once('header.php');
+require_once('footer.php');
+$headr = new Header();
+$headr->isLoggedin();
+
+$footr = new Footer();
+$footr->isLoggedin();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Employer Homepage</title>
+  </head>
+  <body>
+
+  </body>
+</html>
