1403ef24bf5deba174a61ef167cdf401aa2cf6eb
Daniel Simiyu <danielmsimiyu71@gmail.com>
2020-06-04 20:46:13 +0300

    logout update function

--- kenyan_careers_webapp/Employer/employer_logout.php
+++ kenyan_careers_webapp/Employer/employer_logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy();
-header('location: explorepage.php') //explorepage refers to the landing page of our aplication
+// header('location: explorepage.php') //explorepage refers to the landing page of our aplication
  ?>
