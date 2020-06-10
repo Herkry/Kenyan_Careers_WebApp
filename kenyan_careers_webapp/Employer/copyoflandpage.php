b041c3e46aecb711e844df766acec8a8a9a645db
Daniel Simiyu <danielmsimiyu71@gmail.com>
2020-06-10 18:04:27 +0300

    Minor update

---
+++ kenyan_careers_webapp/Employer/copyoflandpage.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Kenyan Career</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+
+    <link rel="stylesheet" href="Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/animate.css">
+
+
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/owl.carousel.min.css">
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/magnific-popup.css">
+
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/aos.css">
+
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/ionicons.min.css">
+
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/flaticon.css">
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/icomoon.css">
+    <link rel="stylesheet" href="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/CSS/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">Kenyan Career<span></span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Upload CV</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Log In</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END nav -->
+    <!--End sliding gallery -->
+
+
+
+    <!--dropdown buttons-->
+
+    <!--Job Availbale -->
+    <section style=" background: #000000FF; opacity: 0.9; padding: 10px 0px 30px 0px; ">
+        <h3 style="text-align: center; color: white;">Top Jobs</h3>
+        <div class="container" style="padding: 15px;">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="dropdown">
+                        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 380px;">All Industries</button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Agriculture, Food and Natural Resources</a>
+                            <a class="dropdown-item" href="#">Architecture and Construction</a>
+                            <a class="dropdown-item" href="#">Education and Training</a>
+                            <a class="dropdown-item" href="#">Government and Public Administration</a>
+                            <a class="dropdown-item" href="#">Hospitality and Tourism</a>
+                            <a class="dropdown-item" href="#">Education and Training</a>
+                            <a class="dropdown-item" href="#">Information Technology</a>
+                            <a class="dropdown-item" href="#">Hospitality and Tourism</a>
+                            <a class="dropdown-item" href="#">Manufacturing</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm">
+                    <div class="dropdown">
+                        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 360px;">Location</button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Nairobi</a>
+                            <a class="dropdown-item" href="#">Mombasa</a>
+                            <a class="dropdown-item" href="#">Kisumu</a>
+                            <a class="dropdown-item" href="#">Nakuru</a>
+                            <a class="dropdown-item" href="#">Machakos</a>
+                            <a class="dropdown-item" href="#">Nyeri</a>
+                            <a class="dropdown-item" href="#">Kakamega</a>
+                            <a class="dropdown-item" href="#">Nyanza</a>
+                            <a class="dropdown-item" href="#">Madera</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm">
+                    <div class="dropdown">
+                        <button class="btn btn-light" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 370px;">Search</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="card" style="width: 380px; height: 230px; margin-bottom: 20px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Admin Assistant Job in Kenya (35K-40K)</h5>
+                            <p class="card-text">Title: Admin Assistant – Law Firm</p>
+                            <p class="card-text">Location: Nairobi</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="card" style="width: 380px; height: 230px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Admin Assistant Job in Kenya (35K-40K)</h5>
+                            <p class="card-text">Title: Admin Assistant – Law Firm</p>
+                            <p class="card-text">Location: Nairobi</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="card" style="width: 380px;  height: 230px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Admin Assistant Job in Kenya (35K-40K)</h5>
+                            <p class="card-text">Title: Admin Assistant – Law Firm</p>
+                            <p class="card-text">Location: Nairobi</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="card" style="width: 380px; height: 230px; margin-bottom: 20px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Admin Assistant Job in Kenya (35K-40K)</h5>
+                            <p class="card-text">Title: Admin Assistant – Law Firm</p>
+                            <p class="card-text">Location: Nairobi</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="card" style="width: 380px; height: 230px; margin-bottom: 20px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Admin Assistant Job in Kenya (35K-40K)</h5>
+                            <p class="card-text">Title: Admin Assistant – Law Firm</p>
+                            <p class="card-text">Location: Nairobi</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="card" style="width: 380px; height: 230px; margin-bottom: 20px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Admin Assistant Job in Kenya (35K-40K)</h5>
+                            <p class="card-text">Title: Admin Assistant – Law Firm</p>
+                            <p class="card-text">Location: Nairobi</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="#" class="btn btn-primary" style="margin-left: 500px;">View More Jobs</a>
+            </div>
+        </div>
+    </section>
+    <!--End of Jobs Available-->
+
+
+        <!--Employer or Seeker-->
+        <section style="padding: 20px;">
+            <h1 style="text-align: center;">Create new Account today!</h1>
+
+            <div class="conainer">
+              <div class="row">
+                <h1><a href="#"><button class="btn btn-primary" id="btn-xl" type="button" name="button">Employer </button></a></h1>
+                &nbsp &nbsp
+                <h1><a href="#"><button class="btn btn-primary" id="btn-xl" type="button" name="button">Employee </button></a></h1>
+
+              </div>
+
+              <style media="screen">
+                .conainer
+                {
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                }
+                #btn-xl {
+                  padding: 10px 20px;
+                  font-size: 20px;
+                  border-radius: 10px;
+                  width:100%;    //Specify your width here
+                }
+              </style>
+
+            </div>
+        </section>
+
+
+    <!--Why choose kenyan career-->
+    <!-- <section>
+        <div class="container-fluid mx-auto mt-5 mb-5 col-12" style="text-align: center">
+            <div class="hd">
+                <h3>Stand Out</h3>
+            </div>
+            <p><small>How Kenyan Career Helps You.</small>
+            </p>
+            <div class="row" style="justify-content: center">
+
+                <div class="card col-md-3 col-12">
+                    <div class="card-content">
+                        <div class="card-body"> <img class="img" src="https://i.imgur.com/S7FJza5.png" />
+                            <div class="shadow"></div>
+                            <div class="card-title"> We're Free </div>
+                            <div class="card-subtitle">
+                                <p> <small class="text-muted">We provide a platform where any person can get a job in any company by clicking a button</small> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card col-md-3 col-12 ml-2">
+                    <div class="card-content">
+                        <div class="card-body"> <img class="img" src="https://i.imgur.com/xUWJuHB.png" />
+                            <div class="card-title"> We're Unbiased </div>
+                            <div class="card-subtitle">
+                                <p> <small class="text-muted"> We give everyone an opportunity according to the skill set they offer</small> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card col-md-3 col-12 ml-2">
+                    <div class="card-content">
+                        <div class="card-body"> <img class="img rck" src="https://i.imgur.com/rG3CGn3.png" />
+                            <div class="card-title"> We Guide you </div>
+                            <div class="card-subtitle">
+                                <p> <small class="text-muted">We help you land your job by getting you a job that suits you and your skill set </small> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!--End of Kenyan Career-->
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/popper.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/bootstrap.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery.easing.1.3.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery.waypoints.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery.stellar.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/owl.carousel.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/aos.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/jquery.animateNumber.min.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/google-map.js"></script>
+    <script src="/Kenyan_Careers_WebApp/kenyan_careers_webapp/Assets/js/main.js"></script>
+
+</body>
+
+</html>
