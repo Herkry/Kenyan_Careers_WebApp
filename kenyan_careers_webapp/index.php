de60b94d240ee481b67033cceff6792a2fdc299b
youngizere <Young.Izere@strathmore.edu>
2020-06-19 12:19:53 +0200

    Allowing the applicant to view the home page and Jobs without logging in

--- kenyan_careers_webapp/index.php
+++ kenyan_careers_webapp/index.php
@@ -1,12 +1,12 @@
 <?php
-// Initialize the session
-session_start();
+
+// session_start();
  
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-  header("location: /Kenyan_Careers_WebApp/kenyan_careers_webapp/applicant_login.php");
-  exit;
-}
+
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//   header("location: /Kenyan_Careers_WebApp/kenyan_careers_webapp/applicant_login.php");
+//   exit;
+// }
 
 ?>
 
